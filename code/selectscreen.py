e599c8d3a4da0ccc0d086aa593f6305ba0e7bc36
Bradley_Bullimore <superbrb2@gmail.com>
2024-01-15 16:30:14 +0000

    Added text to Menu buttons

--- code/selectscreen.py
+++ code/selectscreen.py
@@ -10,9 +10,12 @@ class gameSelect():
         
     def display_menu(self,screen):
         locations = [self.HEIGHT*0.45,(self.HEIGHT*5)/7]
+        menu_options = ['Player Vs Player','Player Vs Computer']
+        font = pygame.font.SysFont('Ariel',75)
         for i in range(2):
-            box_rect = pygame.Rect((self.WIDTH)/14,locations[i],(self.WIDTH*6)/7, self.HEIGHT/7)
+            box_rect = pygame.Rect(self.WIDTH/14,locations[i],(self.WIDTH*6)/7, self.HEIGHT/7)
             pygame.draw.rect(screen,'#F7C59F',box_rect)
+            self.screen.blit(font.render(menu_options[i],True,(255,255,255)),(round(self.WIDTH/14)+25,round(locations[i])+25))
 
         
     def hide_menu(self):
